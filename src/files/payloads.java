da635ef2610b3769489b7f8ffbfd4208dc441463
Jose Becerra <jose.jbv@gmail.com>
2024-10-20 20:15:07 -0300

    Updating code from course

--- src/files/payloads.java
+++ src/files/payloads.java
@@ -47,4 +47,16 @@ public class payloads {
 				"    ]\n" +
 				"}";
 	}
+
+	public static String addBook(String isbn, String aisle){
+		String payload = "{\n" +
+				"\n" +
+				"\n" +
+				"\"name\":\"Learn Appium Automation with Java\",\n" +
+				"\"isbn\":\""+ isbn + "\",\n" +
+				"\"aisle\":\""+ aisle + "\",\n" +
+				"\"author\":\"John foe\"\n" +
+				"}\n";
+		return payload;
+	}
 }
---
+++ src/restAPI/dynamicJson.java
@@ -0,0 +1,35 @@
+package restAPI;
+
+import files.payloads;
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+
+import io.restassured.path.json.JsonPath;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class dynamicJson {
+
+    @Test(dataProvider = "BooksData")
+    public void addBook(String isbn, String aisle) {
+        RestAssured.baseURI = "http://216.10.245.166";
+
+        String response =
+        given().
+            header("Content-Type","application/json").
+            body(payloads.addBook(isbn,aisle)).
+        when().
+            post("Library/Addbook.php").
+        then().
+            assertThat().statusCode(200).
+            extract().response().asString();
+
+        JsonPath js = new JsonPath(response);
+        System.out.println(js.getString("ID"));
+    }
+
+    @DataProvider(name = "BooksData")
+    public Object[][] getData() {
+        return new Object[][] {{"asdfg","1234"},{"qwerty","5678"},{"zxcvb","0987"}};
+    }
+}
