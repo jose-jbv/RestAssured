cf3d36c1c1fbb9ae3ba8572b34b7ee88f403ddac
Jose Becerra <jose.jbv@gmail.com>
2024-10-23 21:56:24 -0300

    Updating code from course

--- src/serialize/Location.java
+++ src/serialize/Location.java
@@ -4,19 +4,19 @@ public class Location {
     private double lat;
     private double lng;
 
-    public int getLat() {
+    public double getLat() {
         return lat;
     }
 
-    public int getLng() {
+    public double getLng() {
         return lng;
     }
 
-    public void setLat(int lat) {
+    public void setLat(double lat) {
         this.lat = lat;
     }
 
-    public void setLng(int lng) {
+    public void setLng(double lng) {
         this.lng = lng;
     }
 }
--- src/serialize/SerializeTest.java
+++ src/serialize/SerializeTest.java
@@ -1,6 +1,7 @@
 package serialize;
 
 import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,10 +18,28 @@ public class SerializeTest {
         place.setPhone_number("123456");
         place.setWebsite("www.kimet.cl");
         place.setName("House");
+
         List<String> myTypes = new ArrayList<String>();
         myTypes.add("a");
         myTypes.add("b");
         place.setTypes(myTypes);
+
+        Location location = new Location();
+        location.setLat(-38.383494);
+        location.setLng(33.427362);
+        place.setLocation(location);
+
+        String response =
+                given().
+                        queryParam("key","qaclick123").
+                        body(place).
+                when().
+                        post("/maps/api/place/add/json").
+                then().
+                        assertThat().statusCode(200).
+                        extract().response().asString();
+
+        System.out.println(response);
 
     }
 }
